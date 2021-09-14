62a989f727b46e878ab800b272cb3dbf82fc06db
Pedro Henrique Sotto-Mayor Pereira da Silva <itapeu16@hotmail.com>
2021-09-13 19:05:18 -0700

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner sc= new Scanner(System.in);
+        String user=" ", pass=" ", def_pass="abc$123";
+
+        System.out.print("What is the username? ");
+        user=sc.nextLine();
+        System.out.print("What is the password? ");
+        pass=sc.nextLine();
+
+        if(pass.equals(def_pass)){
+            System.out.println("Welcome!");
+        }
+        else{
+            System.out.println("I do not know you!");
+        }
+
+
+    }
+}
